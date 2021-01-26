27c2c4ec8191e2fd4d94c96c18ecf35067474d18
Lukas <luki111.1998@gmail.com>
2021-01-26 10:55:11 +0100

    2.1 Dodanie intArrayStack oraz delegacji w defaultCountingOutRhymer

---
+++ src/main/java/edu/kis/vh/nursery/intArrayStack.java
@@ -0,0 +1,56 @@
+package edu.kis.vh.nursery;
+
+/**
+ *
+ */
+public class intArrayStack {
+
+	public static final int NUMBERS_AMOUNT = 12;
+	public static final int DEFAULT_VALUE = -1;
+	public static final int INITIAL_INDEX = -1;
+	public static final int MAX_VALUE = 11;
+	private final int[] numbers = new int[NUMBERS_AMOUNT];
+
+	public int total = INITIAL_INDEX;
+
+	/**
+	 * @param in
+	 */
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean callCheck() {
+			return total == DEFAULT_VALUE;
+		}
+
+	/**
+	 * @return
+	 */
+	public boolean isFull() {
+				return total == MAX_VALUE;
+			}
+
+	/**
+	 * @return
+	 */
+	protected int peekaboo() {
+		if (callCheck())
+			return DEFAULT_VALUE;
+		return numbers[total];
+	}
+
+	/**
+	 * @return
+	 */
+	public int countOut() {
+		if (callCheck())
+			return DEFAULT_VALUE;
+		return numbers[total--];
+	}
+
+}
